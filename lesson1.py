9ec4b290f47b6a2349ded2680913395c93ee3e70
Test Platforms <tintin1880@yahoo.com>
2019-11-13 21:34:42 -0800

    Add L&L Lesson 1

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+
+def main():
+
+    # Variable holding a string
+    message = "Hello and welcome to Lunch and Learn!"
+
+    # Display message to screen
+    print(message)
+
+if __name__ == '__main__':
+    main()
