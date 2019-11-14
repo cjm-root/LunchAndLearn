82337974c69a3e172afd629de428bd239ff171ee
Test Platforms <tintin1880@yahoo.com>
2019-11-14 00:29:18 -0800

    Second iteration + data files

---
+++ lesson2.py
@@ -0,0 +1,23 @@
+
+
+def main():
+
+    print("HEllo world")
+
+# Open file
+# Parse file
+
+# More data structures
+
+# Control structures
+# looping
+
+# list team members in this list per team
+# Most points per team
+
+  with open() as f:
+      read_data = f.read()
+
+  f.close()
+if __name__ == "__main__":
+    main()
